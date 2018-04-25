0c9bbf337524126aadcb64e982bc78077cc36e13
jiangsir <jiangsir@jiangsirde-Air.Home>
2018-04-26 06:35:31 +0800

    save

--- MainGUI.py
+++ MainGUI.py
@@ -138,28 +138,6 @@ def trainNewPersonGUI(text, gifimagepath):
     # Code to add widgets will go here...
     top.mainloop()
 
-# FaceAPI 相關的錯誤。有些可能是如：RateLimit Exceed 用量超過
-# API Key 有錯要更新之類的問題。
-def FaceAPIErrorGUI(text):
-    top = tk.Toplevel()
-    top.geometry('400x400')
-    top.title(text)
-    label = tk.Label(top, text=text, font=('Arial', 20))
-    label.pack()
-
-    #frame = tkinter.Frame(master=top).grid(row=1, column=2)
-    label1 = tk.Label(top, text='', font=('Arial', 18))
-    label1.pack()
-    e = tk.Entry(top, font=("Calibri", 24), width=10, show="")
-    e.pack()
-    e.insert(0, "message: "+ text)
-
-    b2 = tk.Button(
-        top, text='關閉', width=15, height=4, command=top.destroy)
-    b2.pack()
-
-    # Code to add widgets will go here...
-    top.mainloop()
 
 
 def NotMeGUI(top, gifimagepath):
