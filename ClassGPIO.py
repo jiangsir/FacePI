8f326fa27e232cad2a8d5b461193c3c4dbd084d3
jiangsir <jiangsir@jiangsirde-Air.Home>
2018-04-26 19:07:04 +0800

    設定 pin

--- ClassGPIO.py
+++ ClassGPIO.py
@@ -1,19 +1,17 @@
 import RPi.GPIO as GPIO
 
+pin = 18
+GPIO.setmode(GPIO.BCM)  # set board mode to Broadcom
+GPIO.setup(pin, GPIO.OUT)  # set up pin 18
+
 def RelayOn():
-    GPIO.setmode(GPIO.BCM)  # set board mode to Broadcom
-    GPIO.setup(18, GPIO.OUT)  # set up pin 18
-    GPIO.output(18, 1)  # turn on pin 18
+    GPIO.output(pin, 1)  # turn on pin 18
     
 def RelayOff():
-    GPIO.setmode(GPIO.BCM)  # set board mode to Broadcom
-    GPIO.setup(18, GPIO.OUT)  # set up pin 18
-    GPIO.output(18, 0)  # turn off pin 18
+    GPIO.output(pin, 0)  # turn off pin 18
 
 def RelayExchange():
-    GPIO.setmode(GPIO.BCM)  # set board mode to Broadcom
-    GPIO.setup(18, GPIO.OUT)  # set up pin 18
-    if GPIO.input(18):
-        GPIO.output(18, 0)  # turn off pin 18
+    if GPIO.input(pin):
+        GPIO.output(pin, 0)  # turn off pin 18
     else:
-        GPIO.output(18, 1)  # turn on pin 18
+        GPIO.output(pin, 1)  # turn on pin 18
