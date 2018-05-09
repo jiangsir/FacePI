64f52ed5b47089417fddfbb8d58f356846782059
jiangsir <jiangzero@gmail.com>
2018-05-09 15:52:09 +0800

    pinyin

--- ClassGTTS.py
+++ ClassGTTS.py
@@ -1,19 +1,24 @@
 import sys, os
 from gtts import gTTS
 from pygame import mixer
+from pypinyin import lazy_pinyin
+
 
 basepath = '~/mp3/'
 if not os.path.exists(os.path.dirname(basepath)):
     os.makedirs(os.path.dirname(basepath))
 
+
 def play_gTTS(text):
-    tts=gTTS(text=text, lang='zh-tw')
-    mp3path = basepath+text+".mp3"
-    if  os.path.exists(mp3path)==False:
+    tts = gTTS(text=text, lang='zh-tw')
+    text = '_'.join(lazy_pinyin(text))
+    mp3path = basepath + text + ".mp3"
+    print('gTTS:', text, 'mp3path:', mp3path)
+    if os.path.exists(mp3path) == False:
         tts.save(mp3path)
-    os.system('omxplayer '+mp3path)
+    os.system('omxplayer ' + mp3path)
+
 
 #mixer.init()
 #mixer.music.load(mp3path)
 #mixer.music.play()
-
--- README.md
+++ README.md
@@ -185,10 +185,10 @@ confidence|	Number|	信心指數從 0 ~ 1
 
 本 Python 程式執行前，有幾個套件需要事先準備好：
 
-    pip install fire
+    pip3 install fire
     sudo pip3 install gTTS # 這裡需要 sudo
     pip3 install pygame
-    pip install pypinyin
+    sudo pip3 install pypinyin
     
 
 執行主畫面：
