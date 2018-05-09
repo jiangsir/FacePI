fb9e887fe1d811eb015191ac60fa65a63753ec63
jiangsir <jiangzero@gmail.com>
2018-05-09 15:57:10 +0800

    中文

--- ClassGTTS.py
+++ ClassGTTS.py
@@ -11,7 +11,7 @@ if not os.path.exists(os.path.dirname(basepath)):
 
 def play_gTTS(text):
     tts = gTTS(text=text, lang='zh-tw')
-    text = '_'.join(lazy_pinyin(text))
+    #text = '_'.join(lazy_pinyin(text))
     mp3path = basepath + text + ".mp3"
     print('gTTS:', text, 'mp3path:', mp3path)
     if os.path.exists(mp3path) == False:
