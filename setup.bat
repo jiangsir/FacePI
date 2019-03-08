dd49c20ee9e2acd616385acd9c879e9cfd655595
jiangsir <jiangzero@gmail.com>
2019-03-08 14:32:20 +0800

    ==

--- setup.bat
+++ setup.bat
@@ -2,7 +2,7 @@ call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
 call conda create -n cv3 python=3.5.2 -y
 call conda activate cv3
 rem opencv 4.00 前的最後一版
-call pip install opencv-python=3.4.5.20
+call pip install opencv-python==3.4.5.20
 call pip install fire Pillow pypinyin django pandas pymysql numpy scipy scikit-image cmake dlib imutils
 
 rem dlib 另一個安裝版本
