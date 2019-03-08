672783194a0644782eef4bcc303e5a23b5ca2d74
jiangsir <jiangzero@gmail.com>
2019-03-08 10:54:35 +0800

    %CD%\FacePI.py 方便移植

--- RunFacePI_Train.bat
+++ RunFacePI_Train.bat
@@ -1,5 +1,5 @@
 call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
 call conda activate cv3
-call python C:\Users\jiangsir\Documents\FacePI\FacePI.py Train
+call python %CD%\FacePI.py Train
 rem python C:\Users\user\Documents\FacePI\website\manage.py runserver 0.0.0.0:8000
 rem call conda deactivate
--- RunFacePI_runserver.bat
+++ RunFacePI_runserver.bat
@@ -1,5 +1,5 @@
 call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
 call conda activate cv3
-call python C:\Users\jiangsir\Documents\FacePI\website\manage.py runserver 0.0.0.0:8000
+call python %CD%\website\manage.py runserver 0.0.0.0:8000
 rem call conda deactivate
 pause
