2495100fab7bee7fe48ff08d8d9f0b980cb6412a
jiangsir <jiangzero@gmail.com>
2019-03-08 15:03:38 +0800

    bat call anaconda

--- setup.bat
+++ setup.bat
@@ -1,9 +1,19 @@
+
+IF EXIST C:\ProgramData\Anaconda3 (
 call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
+) 
+ELSE (
+call %HomeDrive%%HomePath%\Anaconda3\Scripts\activate.bat %HomeDrive%%HomePath%\Anaconda3
+)
+
 call conda create -n cv3 python=3.5.2 -y
 call conda activate cv3
-rem opencv 4.00 前的最後一版
-call pip install opencv-python==3.4.5.20
-call pip install fire Pillow pypinyin django pandas pymysql numpy scipy scikit-image cmake dlib imutils
+call conda install -c menpo opencv3
+
+rem rem opencv 4.00 前的最後一版
+rem call pip install opencv-python==3.4.5.20
+
+call pip install fire Pillow pypinyin django pandas pymysql numpy scipy scikit-image cmake dlib==18.18 imutils
 
 rem dlib 另一個安裝版本
 rem pip install pip install dlib==18.17.100
