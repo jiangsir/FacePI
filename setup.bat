3ba790302e074cbc4cc2d17f0ceae8fa8c6c44c3
jiangsir <jiangzero@gmail.com>
2019-03-08 17:53:46 +0800

    conda remove cv3

--- setup.bat
+++ setup.bat
@@ -1,25 +1,36 @@
+setlocal enableextensions enabledelayedexpansion
 
-IF EXIST "C:\ProgramData\Anaconda3\" (
-call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
+
+IF EXIST %ALLUSERSPROFILE%\Anaconda3\ (
+call %ALLUSERSPROFILE%\Anaconda3\Scripts\activate.bat %ALLUSERSPROFILE%\Anaconda3
+set ANACONDA_PATH=%ALLUSERSPROFILE%\Anaconda3
 echo alluser
 ) ELSE (
 call %HomeDrive%%HomePath%\Anaconda3\Scripts\activate.bat %HomeDrive%%HomePath%\Anaconda3
+set ANACONDA_PATH=%HomeDrive%%HomePath%\Anaconda3
 echo one user
 )
 
+SET PATH=%PATH%;%ANACONDA_PATH%;%ANACONDA_PATH%\Scripts\;
+
+call conda remove -n cv3 --all
 call conda create -n cv3 python=3.5.2 -y
 call conda activate cv3
 rem call conda install -c menpo opencv3 -y
 
-rem rem opencv 4.00 前的最後一版
+python -m pip install --upgrade pip
+rem rem opencv 4.00
 call pip install opencv-python==3.2.0.8
 
-call pip install fire Pillow pypinyin django pandas pymysql numpy scipy scikit-image cmake imutils
+call pip install cython msgpack fire Pillow pypinyin django pandas pymysql numpy scipy scikit-image cmake imutils
 
-rem dlib 另一個安裝版本
-pip install pip install dlib==18.17.100
+rem dlib
+pip install dlib==18.17.100
+pip install dlib
 
-rem 處理圖片
+rem
 rem pip install imutils
 
 rem call conda deactivate
+pause
+
