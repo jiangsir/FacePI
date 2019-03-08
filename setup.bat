033e25e4d084a3c8dbbc7b863f2f1941142d77a8
jiangsir <jiangzero@gmail.com>
2019-03-08 16:49:07 +0800

    setup

--- setup.bat
+++ setup.bat
@@ -1,22 +1,24 @@
 
-IF EXIST C:\ProgramData\Anaconda3 (
+IF EXIST C:\ProgramData\Anaconda3\ (
 call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
-) 
-ELSE (
+echo alluser
+) ELSE (
 call %HomeDrive%%HomePath%\Anaconda3\Scripts\activate.bat %HomeDrive%%HomePath%\Anaconda3
+echo one user
 )
 
 call conda create -n cv3 python=3.5.2 -y
 call conda activate cv3
-call conda install -c menpo opencv3
+rem call conda install -c menpo opencv3 -y
 
 rem rem opencv 4.00 前的最後一版
-rem call pip install opencv-python==3.4.5.20
+call pip install opencv-python==3.2.0.8
 
-call pip install fire Pillow pypinyin django pandas pymysql numpy scipy scikit-image cmake dlib==18.18 imutils
+call pip install fire Pillow pypinyin django pandas pymysql numpy scipy scikit-image cmake imutils
 
 rem dlib 另一個安裝版本
-rem pip install pip install dlib==18.17.100
+pip install pip install dlib==18.17.100
+
 rem 處理圖片
 rem pip install imutils
 
